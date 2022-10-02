ed1f978c01a9be835bfa42190031ca23c536a3c8
JubayerDev <ajashes858@gmail.com>
2022-10-02 16:22:44 +0600

    some error handling

--- app.js
+++ app.js
@@ -7,15 +7,15 @@ function selectBtn(playerName) {
 
   const namesLength = document.getElementsByTagName("li");
   if (namesLength.length >= 5) {
-    alert(
-      "Warning!! You already added five Player. You cannot more than that."
-    );
     document.getElementById("btn-1").setAttribute("disabled", "");
     document.getElementById("btn-2").setAttribute("disabled", "");
     document.getElementById("btn-3").setAttribute("disabled", "");
     document.getElementById("btn-4").setAttribute("disabled", "");
     document.getElementById("btn-5").setAttribute("disabled", "");
     document.getElementById("btn-6").setAttribute("disabled", "");
+    alert(
+      "Warning!! You already added five Player. You cannot more than that."
+    );
   }
 }
 
@@ -64,15 +64,17 @@ document.getElementById("btn-6").addEventListener("click", function () {
 document.getElementById("btn-calculate").addEventListener("click", function () {
   const playerLists = document.getElementsByTagName("li").length;
   const playerField = document.getElementById("per-player-flield").value;
-    const playerExpense = document.getElementById("player-expense");
-    if (playerLists < 1) {
-        return alert('Please Add Five Players')
-    }
+  const playerExpense = document.getElementById("player-expense");
+  if (playerLists < 1) {
+    return alert("Please Add Five Players");
+  }
   const playerBudget = playerLists * playerField;
   playerExpense.innerText = playerBudget;
 });
 
-document.getElementById("calculate-total-btn").addEventListener("click", function () {
+document
+  .getElementById("calculate-total-btn")
+  .addEventListener("click", function () {
     const playerLists = document.getElementsByTagName("li").length;
     const playerField = document.getElementById("per-player-flield").value;
     const playerBudget = playerLists * playerField;
@@ -83,7 +85,7 @@ document.getElementById("calculate-total-btn").addEventListener("click", functio
     const manager = parseFloat(managerInput.value);
     const coach = parseFloat(coachInput.value);
 
-    if ( typeof managerInput !== 'number' || typeof coachInput == 'number') {
+    if (typeof managerInput !== "number" || typeof coachInput == "number") {
       alert("Name must be filled out");
       return false;
     } else {
